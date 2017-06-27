6732f89181c5b6946220195d34abdf1430e5dadf
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-06-27 12:31:30 +0100

    Add static 'Add Pokemon' form

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, PreloadAllModules } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PokemonAddComponent } from './poke-add/poke-add.component';
 
 @NgModule({
     imports: [
         RouterModule.forRoot([
             { path: '', component: HomeComponent },
+            { path: 'add-pokemon', component: PokemonAddComponent },
             { path: '**', redirectTo: '', pathMatch: 'full' }
         ] , { preloadingStrategy: PreloadAllModules })
     ],
